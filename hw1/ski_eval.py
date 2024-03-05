4d09d3eb2e8886b36eca500be9ddfec7ffc11fb6
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-03-05 00:45:56 -0500

    use ludwig's solutions to implement the ski interpreter

--- hw1/ski_eval.py
+++ hw1/ski_eval.py
@@ -6,7 +6,75 @@ import src.ski as ski
 ##########
 # TASK: Implement the below function `eval`.
 
-def eval(e: ski.Expr) -> ski.Expr:
-    # BEGIN_YOUR_CODE
-    return e
-    # END_YOUR_CODE
+
+def eval(expression: ski.Expr) -> ski.Expr:
+    seen = set()
+    while expression not in seen:
+        seen.add(expression)
+        expression = rewrite(expression)
+    return expression
+
+
+# def rewrite(e: ski.Expr) -> ski.Expr:
+#     if not isinstance(e, ski.App):
+#         return e
+#     e.e1 = rewrite(e.e1)
+#     e.e2 = rewrite(e.e2)
+#     match e.e1:
+#         case ski.I():
+#             # I e → e
+#             return e.e2
+#         case ski.App(e1=ski.K()):
+#             # K e1 e2 → e1
+#             return e.e1.e2
+#         case ski.App(e1=ski.App(e1=ski.S())):
+#             # S e1 e2 e3 → (e1 e3) (e2 e3).
+#             return ski.App(
+#                 e1=ski.App(e.e1.e1.e2, e.e2),
+#                 e2=ski.App(e.e1.e2, e.e2),
+#             )
+#         case _:
+#             return e
+
+
+def rewrite(expression: ski.Expr) -> ski.Expr:
+    match expression:
+        case e1, e2:
+            match rewrite(e1), rewrite(e2):
+                case "I", e1:
+                    # I e1 → e1
+                    return e1
+                case ("K", e1), e2:
+                    # K e1 e2 → e1
+                    return e1
+                case (("S", e1), e2), e3:
+                    # S e1 e2 e3 → (e1 e3) (e2 e3)
+                    return (e1, e3), (e2, e3)
+                case rewritten:
+                    return rewritten
+        case _:
+            return expression
+
+
+def representation(expression):
+    match expression:
+        case (e1, e2):
+            return f"({representation(e1)} {representation(e2)})"
+        case other:
+            return other
+
+
+inc = ("S", (("S", ("K", "S")), "K"))
+_0 = ("S", "K")
+_1 = (inc, _0)
+
+for expression, expected_output in [
+    [((_0, "f"), "x"), "x"],
+    [((_1, "f"), "x"), "(f x)"],
+    [(((inc, _1), "f"), "x"), "(f (f x))"],
+]:
+    reduced = eval(expression)
+    print(f"Before:   {representation(expression)}")
+    print(f"After:    {representation(reduced)}")
+    print(f"Expected: {expected_output}\n")
+    assert representation(reduced) == expected_output
